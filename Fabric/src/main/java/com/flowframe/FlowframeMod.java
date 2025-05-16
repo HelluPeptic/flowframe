aa67bc213fb667267fa71ca10b5b25057b37b3ca
OfficialPeptic <alex06sjodin@gmail.com>
2025-05-16 16:10:01 +0200

    Added all working changes

--- Fabric/src/main/java/com/flowframe/FlowframeMod.java
+++ Fabric/src/main/java/com/flowframe/FlowframeMod.java
@@ -1,17 +1,20 @@
 package com.flowframe;
 
 import net.fabricmc.api.ModInitializer;
+
 import com.flowframe.features.oreannounce.OreAnnounceFeature;
 import com.flowframe.features.keepinventory.KeepInventoryFeature;
 import com.flowframe.features.creepernogrief.CreeperNoGriefFeature;
+import com.flowframe.features.tphere.TpHereCommand;
 
 public class FlowframeMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        System.out.println("[Flowframe] Initializing Flowframe mod");
+        System.out.println("[FLOWFRAME] Initializing Flowframe mod");
         // Register features here:
         OreAnnounceFeature.register();
         KeepInventoryFeature.register(); 
         CreeperNoGriefFeature.register();
+        TpHereCommand.register();
     }
 }
