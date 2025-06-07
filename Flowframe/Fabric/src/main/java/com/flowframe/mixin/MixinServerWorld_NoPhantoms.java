38484a1b5614abe76530e50e16e94d7c110e7997
OfficialPeptic <alex06sjodin@gmail.com>
2025-06-07 15:50:42 +0200

    Added all working changes

--- Flowframe/Fabric/src/main/java/com/flowframe/mixin/MixinServerWorld_NoPhantoms.java
+++ Flowframe/Fabric/src/main/java/com/flowframe/mixin/MixinServerWorld_NoPhantoms.java
@@ -3,6 +3,7 @@ package com.flowframe.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MixinServerWorld_NoPhantoms {
     @Inject(method = "spawnEntity", at = @At("HEAD"), cancellable = true)
     private void denyPhantomEntitySpawn(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        ServerWorld serverWorld = (ServerWorld) (Object) this;
         if (entity.getType() == EntityType.PHANTOM) {
-            cir.setReturnValue(false); // Cancel phantom spawn
+            if (serverWorld.getRegistryKey().getValue().equals(new Identifier("minecraft", "overworld"))) {
+                cir.setReturnValue(false); // Cancel phantom spawn in the overworld
+            }
+            // Do nothing in the nether or other dimensions, let vanilla handle it
         }
     }
 }
